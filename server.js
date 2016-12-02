57de9a8fa60b6a13c6ae015e93c53835e19108ba
Inno Estrera <inno.irving.estrera@gmail.com>
2016-12-01 16:36:46 -0800

    getting rid of CORS issue

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
 require('app-module-path').addPath(__dirname);
 
 const app = require('koa')();
+const cors = require('koa-cors');
 const product = require('src/product/route');
+
+app.use(cors())
 
 // register api routes
 app.use(product.routes());
